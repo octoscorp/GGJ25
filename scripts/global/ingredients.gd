c48e94afc87928497e445103c360711a58396b7c
octoscorp <gfthampton@gmail.com>
2025-01-25 15:28:17 +1300

    SHRIMP
    Shrimp shrimpy shrimp, shrimping shrimping shrimps.
    
    Added generic ingredient class (defaults to shrimp), beginnings of Sprite
    and cooking settings.

---
+++ scripts/global/ingredients.gd
@@ -0,0 +1,19 @@
+extends Node
+
+enum {
+	SHRIMP,
+	CARROT,
+}
+
+const SHAPES = {
+	SHRIMP: [
+		Vector2(-8, 33),
+		Vector2(14, 4),
+		Vector2(11, -65),
+		Vector2(63, -63),
+		Vector2(63, -1),
+		Vector2(25, 62),
+		Vector2(-25, 61),
+		Vector2(-62, 20),
+	],
+}
