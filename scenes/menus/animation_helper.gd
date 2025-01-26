e546abf882037f567e859500aaad963fd19f83d9
Hayden Leete <haydenleete@gmail.com>
2025-01-26 16:09:22 +1300

    integrate tutorial

--- scenes/menus/animation_helper.gd
+++ scenes/menus/animation_helper.gd
@@ -13,3 +13,4 @@ func appear():
 	show()
 	tween.tween_property(self, "rotation", 0, 0.5).from(0.1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
 	tween.parallel().tween_property(self, "scale", Vector2.ONE, 0.2).from(Vector2.ONE*0.9).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	await tween.finished
--- scenes/menus/tutorial_sequencer.gd
+++ scenes/menus/tutorial_sequencer.gd
@@ -3,13 +3,15 @@ extends Node2D
 @onready var chatter_sfx: AudioStreamPlayer = $ChatterSFX
 
 @onready var events = [
-	auto_next_event.bind(
-		play_anim.bind("intro")),
+	auto_next_event.bind($TextBox.appear),
 	animate_text.bind(%SpeechLabel, "Cookin shrimp? That's my fav! The pot needs to boil. Click and drag the handle to move the pot."),
 	animate_text.bind(%SpeechLabel, "The shrimp is not quite done yet, its gota be juuust right. ^u^"),
 	animate_text.bind(%SpeechLabel, "It's done now, scoop out onto a plate. *u*"),
+	auto_next_event.bind(animate_text.bind(%SpeechLabel, "Good shrimp :)")),
+	finish
 	]
 
+var finished = false
 
 var event_index = 0
 var cut_off = false
@@ -19,7 +21,11 @@ var text_label : Label
 
 func _ready() -> void:
 	next_event()
-	
+
+func finish():
+	finished = true
+	$TextureButton/RestartButton.text = "Menu"
+	$TextureButton.self_modulate = Color.GREEN
 
 #func _input(event: InputEvent) -> void:
 #	if event.is_action_pressed("next_event"):
@@ -27,9 +33,11 @@ func _ready() -> void:
 #		cut_off = true
 
 func next_event():
+	print("LET'S GO") 
 	if event_index < events.size():
-		events[event_index].call()
-	event_index += 1
+		event_index += 1
+		events[event_index-1].call()
+	
 
 func auto_next_event(event : Callable):
 	await event.call()
@@ -62,3 +70,13 @@ func delay(seconds):
 
 func _on_shrimp_cook_progress() -> void:
 	next_event()
+
+
+func _on_dropoff_area_no_ingredients_left() -> void:
+	next_event()
+
+func _on_restart_button_pressed() -> void:
+	if finished:
+		get_tree().change_scene_to_file("res://scenes/menus/StartMenu.tscn")
+	else:
+		get_tree().reload_current_scene()
